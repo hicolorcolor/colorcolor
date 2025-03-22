1b47a57b1ec8777954b109b161c372af4c698257
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-03-22 16:43:26 +0900

    렌더 포트 등록

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
+    port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
