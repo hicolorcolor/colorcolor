c41d14b492f41d5b727556e57e241649cedd2ae9
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-03-26 18:58:47 +0900

    디테일 페이지 수정

--- frontend/components/ColorList.jsx
+++ frontend/components/ColorList.jsx
@@ -78,7 +78,7 @@ const ColorList = () => {
     useEffect(() => {
         setFade(false); // 변경 시 먼저 투명하게
         setTimeout(() => setFade(true), 300); // 0.1초 뒤 페이드인 효과 적용
-    }, [typeFilter, colorFilter, search]); // 필터 변경 시 트랜지션 적용
+    }, [typeFilter, colorFilter, search, queryParams]); // 필터 변경 시 트랜지션 적용
 
     return (
         <Layout
